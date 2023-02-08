71d72c8ac14667fb793ebe76732cf8b0bfdc2e88
Evgeniy <evgeniygal@gmail.com>
2023-02-08 16:11:29 +0200

    Initial commit

---
+++ src/main/java/edu/goit/telegrambot/CurrencyBot.java
@@ -0,0 +1,47 @@
+package edu.goit.telegrambot;
+
+import edu.goit.telegrambot.constant.Constants;
+import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.objects.Ability;
+
+import static org.telegram.abilitybots.api.objects.Locality.ALL;
+import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
+
+public class CurrencyBot extends AbilityBot {
+
+    protected CurrencyBot() {
+        super(Constants.BOT_TOKEN, Constants.BOT_USERNAME);
+    }
+
+    protected CurrencyBot(String botToken, String botUsername) {
+        super(botToken, botUsername);
+    }
+
+    @Override
+    public long creatorId() {
+        return Constants.CREATOR_ID;
+    }
+
+    public Ability sayHelloWorld() {
+        return Ability
+                .builder()
+                .name("hello")
+                .info("says hello world!")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> silent.send("Hello world!", ctx.chatId()))
+                .build();
+    }
+
+    public Ability command1() {
+        return Ability
+                .builder()
+                .name("command1")
+                .info("says hello world!")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> silent.send("Hello from command1!!!!!", ctx.chatId()))
+                .build();
+    }
+
+}
---
+++ src/main/java/edu/goit/telegrambot/Main.java
@@ -0,0 +1,23 @@
+package edu.goit.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            // Create the TelegramBotsApi object to register your bots
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+            // Register your newly created AbilityBot
+            botsApi.registerBot(new CurrencyBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
