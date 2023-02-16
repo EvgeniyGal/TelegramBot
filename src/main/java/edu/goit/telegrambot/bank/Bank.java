f0d6205538a2a4d909755f317a7adb469863322d
Ivan Lukash <ivlukash@ukr.net>
2023-02-16 19:51:56 +0200

    create getCurrencyRate()

--- src/main/java/edu/goit/telegrambot/bank/Bank.java
+++ src/main/java/edu/goit/telegrambot/bank/Bank.java
@@ -1,5 +1,32 @@
 package edu.goit.telegrambot.bank;
 
+import edu.goit.telegrambot.currency.Currency;
+import edu.goit.telegrambot.currency.CurrencyType;
+import java.math.RoundingMode;
+import java.util.List;
+
 public abstract class Bank {
+
+    String name;
+    List<Currency> currencies;
+
+    abstract void updateRate();
+
+    String getCurrencyRate(CurrencyType curType, byte tolerance) {
+        return String.format("Курс валют в %s: %s/%s\n", name, curType.name(), "UAH") +
+                "Купівля: " + currencies.stream()
+                .filter(item -> item.getType().equals(curType))
+                .map(it -> it.getBuyRate().setScale(tolerance, RoundingMode.HALF_UP))
+                .findFirst().orElseThrow() +
+                "\nПродаж: " + currencies.stream()
+                .filter(item -> item.getType().equals(curType))
+                .map(it -> it.getSellRate().setScale(tolerance, RoundingMode.HALF_UP))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
