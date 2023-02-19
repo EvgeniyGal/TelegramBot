5a5b25e53e550633cd65a29cfd3f8c0bbcb805ab
OlgaZar <116227489+OlgaZar@users.noreply.github.com>
2023-02-19 17:57:29 +0200

    commit nbu

--- src/main/java/edu/goit/telegrambot/bank/Test.java
+++ src/main/java/edu/goit/telegrambot/bank/Test.java
@@ -7,5 +7,10 @@ public class Test {
         Bank bank = MonoBank.getInstance();
         System.out.println(bank.getCurrencyRate(CurrencyType.EUR, (byte) 2));
         System.out.println(bank.getCurrencyRate(CurrencyType.USD, (byte) 4));
+
+        Bank bank1= NBUBank.getInstance();
+        System.out.println(bank1.getCurrencyRate(CurrencyType.EUR, (byte) 2));
+        System.out.println(bank1.getCurrencyRate(CurrencyType.USD, (byte) 4));
+
     }
 }
